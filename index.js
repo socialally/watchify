5527eee61a4213196a88b9e831a6dad9aea2a583
cyberfunk <hello@cyberfunk.org>
2015-02-22 12:12:32 +0800

    Allow setting usePolling option.
    Via an environment variable, fixes an issue on OSX Snow Leopard whereby
    the `change` event was not firing.

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ module.exports.args = {
 
 function watchify (b, opts) {
     if (!opts) opts = {};
+    var usePolling = process.env.WATCH_USE_POLLING !== undefined;
     var cache = b._options.cache;
     var pkgcache = b._options.packageCache;
     var changingDeps = {};
     var pending = false;
-    
+
     b.on('dep', function (dep) {
         if (typeof dep.id === 'string') {
             cache[dep.id] = dep;
@@ -23,25 +24,25 @@ function watchify (b, opts) {
             watchFile(dep.file);
         }
     });
-    
+
     b.on('file', function (file) {
         watchFile(file);
     });
-    
+
     b.on('package', function (pkg) {
         watchFile(path.join(pkg.__dirname, 'package.json'));
     });
-    
+
     b.on('reset', reset);
     reset();
-    
+
     function reset () {
         var time = null;
         var bytes = 0;
         b.pipeline.get('record').on('end', function () {
             time = Date.now();
         });
-        
+
         b.pipeline.get('wrap').push(through(write, end));
         function write (buf, enc, next) {
             bytes += buf.length;
@@ -58,29 +59,29 @@ function watchify (b, opts) {
             this.push(null);
         }
     }
-    
+
     var fwatchers = {};
     var fwatcherFiles = {};
-    
+
     b.on('transform', function (tr, mfile) {
         tr.on('file', function (file) {
             watchDepFile(mfile, file);
         });
     });
-    
+
     function watchFile (file) {
         fs.lstat(file, function (err, stat) {
             if (err || stat.isDirectory()) return;
             watchFile_(file);
         });
     }
-    
+
     function watchFile_ (file) {
         if (!fwatchers[file]) fwatchers[file] = [];
         if (!fwatcherFiles[file]) fwatcherFiles[file] = [];
         if (fwatcherFiles[file].indexOf(file) >= 0) return;
-        
-        var w = chokidar.watch(file, {persistent: true});
+
+        var w = chokidar.watch(file, {persistent: true, usePolling: usePolling});
         w.setMaxListeners(0);
         w.on('error', b.emit.bind(b, 'error'));
         w.on('change', function () {
@@ -89,13 +90,13 @@ function watchify (b, opts) {
         fwatchers[file].push(w);
         fwatcherFiles[file].push(file);
     }
-    
+
     function watchDepFile(mfile, file) {
         if (!fwatchers[mfile]) fwatchers[mfile] = [];
         if (!fwatcherFiles[mfile]) fwatcherFiles[mfile] = [];
         if (fwatcherFiles[mfile].indexOf(file) >= 0) return;
 
-        var w = chokidar.watch(file, {persistent: true});
+        var w = chokidar.watch(file, {persistent: true, usePolling: usePolling});
         w.setMaxListeners(0);
         w.on('error', b.emit.bind(b, 'error'));
         w.on('change', function () {
@@ -104,7 +105,7 @@ function watchify (b, opts) {
         fwatchers[mfile].push(w);
         fwatcherFiles[mfile].push(file);
     }
-    
+
     function invalidate (id) {
         if (cache) delete cache[id];
         if (fwatchers[id]) {
@@ -115,22 +116,22 @@ function watchify (b, opts) {
             delete fwatcherFiles[id];
         }
         changingDeps[id] = true
-        
+
         // wait for the disk/editor to quiet down first:
         if (!pending) setTimeout(function () {
             pending = false;
             b.emit('update', Object.keys(changingDeps));
             changingDeps = {};
-        
+
         }, opts.delay || 600);
         pending = true;
     }
-    
+
     b.close = function () {
         Object.keys(fwatchers).forEach(function (id) {
             fwatchers[id].forEach(function (w) { w.close() });
         });
     };
-    
+
     return b;
 }
